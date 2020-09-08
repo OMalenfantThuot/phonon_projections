f11e60f9295d0fd2d5ee1f4357da417ce30d3346
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2020-09-08 14:30:16 -0400

    utilisty for normal modes

---
+++ phonon_projections/utils/normal_modes.py
@@ -0,0 +1,28 @@
+import numpy as np
+from mlcalcdriver import Posinp
+from mlcalcdriver.calculators import SchnetPackCalculator
+from mlcalcdriver.workflows import Phonon
+import h5py
+
+def get_normal_modes(posinp, model, rotate=True):
+    calculator = SchnetPackCalculator(model)
+    posinp = Posinp.from_file(posinp)
+    ph = Phonon(posinp=posinp, calculator=calculator)
+    ph.run()
+    
+    if rotate:
+        ndim = 3 * len(posinp)
+        final_modes = np.zeros((ndim, ndim))
+        
+        idx = np.arange(ndim)
+        for i in range(ndim):
+            init = ph.normal_modes[:,i]
+            final = np.empty_like(init)
+            final[np.where(idx%3 == 0)[0]] = init[np.where(idx%3 == 0)[0]]
+            final[np.where(idx%3 == 1)[0]] = init[np.where(idx%3 == 2)[0]]
+            final[np.where(idx%3 == 2)[0]] = -1.0 * init[np.where(idx%3 == 1)[0]]
+            final_modes[:,i] = final
+        return final_modes
+
+    else:
+        return ph.normal_modes
